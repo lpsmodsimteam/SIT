c2ee6928ba489a4c728146c005220424a404522d
sabbirahm3d <ahmed_sabbir@bah.com>
2018-11-02 10:02:19 -0400

    change to 8-bit counter

--- dev/kernels/systemc/systemc-2.3.2/examples/sysc/counter/counter_tb.cpp
+++ dev/kernels/systemc/systemc-2.3.2/examples/sysc/counter/counter_tb.cpp
@@ -5,7 +5,7 @@ int sc_main(int argc, char *argv[]) {
     sc_signal<bool> clock;
     sc_signal<bool> reset;
     sc_signal<bool> enable;
-    sc_signal<sc_uint<4> > counter_out;
+    sc_signal<sc_uint<8> > counter_out;
     int i = 0;
     // Connect the DUT
     counter counter("COUNTER");
@@ -26,7 +26,7 @@ int sc_main(int argc, char *argv[]) {
         sc_start(1, SC_NS);
     }
     reset = 1;    // Assert the reset
-    cout << "@" << sc_time_stamp() << " Asserting reset\n" << '\n';
+    cout << "@" << sc_time_stamp() << " Asserting reset\n\n";
     for (i = 0; i < 10; i++) {
         clock = 0;
         sc_start(1, SC_NS);
@@ -34,25 +34,25 @@ int sc_main(int argc, char *argv[]) {
         sc_start(1, SC_NS);
     }
     reset = 0;    // De-assert the reset
-    cout << "@" << sc_time_stamp() << " De-Asserting reset\n" << '\n';
+    cout << "@" << sc_time_stamp() << " De-Asserting reset\n\n";
     for (i = 0; i < 5; i++) {
         clock = 0;
         sc_start(1, SC_NS);
         clock = 1;
         sc_start(1, SC_NS);
     }
-    cout << "@" << sc_time_stamp() << " Asserting Enable\n" << '\n';
+    cout << "@" << sc_time_stamp() << " Asserting Enable\n\n";
     enable = 1;  // Assert enable
-    for (i = 0; i < 20; i++) {
+    for (i = 0; i < 260; i++) {
         clock = 0;
         sc_start(1, SC_NS);
         clock = 1;
         sc_start(1, SC_NS);
     }
-    cout << "@" << sc_time_stamp() << " De-Asserting Enable\n" << '\n';
+    cout << "@" << sc_time_stamp() << " De-Asserting Enable\n\n";
     enable = 0; // De-assert enable
 
-    cout << "@" << sc_time_stamp() << " Terminating simulation\n" << '\n';
+    cout << "@" << sc_time_stamp() << " Terminating simulation\n\n";
 
     return 0;// Terminate simulation
 
@@ -65,5 +65,7 @@ int main(int argc, char *argv[]) {
 
     // invoke wrapper - both can't be called
     // sc_main(argc, argv);
+
+    return EXIT_SUCCESS;
 
 }
