8a6c25464d1c65923df1d884a3c22729758bdf59
sabbirahm3d <ahmed_sabbir@bah.com>
2019-02-12 14:34:15 -0500

    add skeleton for src

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
-# SystemC-SST Interoperability
+# SystemC-SST Interoperability Library
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/88c38abb1f2a4a369b4a6f9c49e8d237)](https://www.codacy.com/app/sabbirahm3d/systemc-sst?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=sabbirahm3d/systemc-sst&amp;utm_campaign=Badge_Grade)
 
-## Kernels
+A library to provide interoperability between
+[Structural Simulation Toolkit (SST) 8.0.0](https://github.com/sstsimulator/sst-core)
+(with [SST Elements 8.0.0](https://github.com/sstsimulator/sst-elements)) and
+[SystemC 2.3.2](http://www.accellera.org/downloads/standards/systemc)
+(with [Transaction-Level Modeling Library (TLM 2.0.3)](https://www.doulos.com/knowhow/systemc/tlm2/))
 
-- [SystemC 2.3.2](http://www.accellera.org/downloads/standards/systemc), with [Transaction-Level Modeling Library (TLM 2.0.3)](https://www.doulos.com/knowhow/systemc/tlm2/)
-- [Structural Simulation Toolkit (SST) 8.0.0](https://github.com/sstsimulator/sst-core), with [SST Elements 8.0.0](https://github.com/sstsimulator/sst-elements)
+## Requirements
 
-## Environment
+`src/CMakeLists.txt` contains all the requirements to build the library. 
 
-- C++: `std=c++14` 
+## Build the prototype
+Run `make` on the top level Makefile
 
-- Compiler: `g++ 7.3.0` or `clang 6.0.0-1ubuntu2` 
-
-- CMake 3.13
-
-- SystemC
-
-    - `SYSTEMC_FLAGS = -I. -I${SYSTEMC_HOME}/include -L. -L${SYSTEMC_HOME}/lib-linux64 -Wl,-rpath=${SYSTEMC_HOME}/lib-linux64 -lsystemc -lm`
-
-- SST
-
-    - `ELEMENT_CXXFLAGS = -D__STDC_FORMAT_MACROS -fPIC -DHAVE_CONFIG_H -I${SST_ELEMENTS_HOME}/include`
-    - `ELEMENT_LDFLAGS = -shared -fno-common -Wl,-undefined -Wl,dynamic_lookup`
+The default recipe will create a `build` directory and run the project's CMake. This will build all the development
+SystemC executables and the SST shared objects.
---
+++ src/jsonbuf.hpp
@@ -0,0 +1,87 @@
+#ifndef JSON_BUF_HPP
+#define JSON_BUF_HPP
+
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+#include <sstream>
+#include <unistd.h>
+
+// keep this just for dev
+#include <iostream>
+#include "cprint.hpp"
+
+Color::Modifier RED(Color::FG_RED);
+Color::Modifier BLUE(Color::FG_BLUE);
+Color::Modifier DEF(Color::FG_DEFAULT);
+
+#define BUFSIZE 1025
+
+template<typename T>
+std::string to_string(const T &value) {
+
+    std::ostringstream ss;
+    ss << value;
+    return ss.str();
+
+}
+
+
+void send_json(const json &data, int sock_fd) {
+
+    // convert JSON object to bytes to be transmitted via sockets
+    std::string data_str = to_string(data);
+    ulong buf_size = data_str.size();
+
+//    std::cout << getpid() << " SENDING BEFORE: " << data_str << std::endl;
+    if (write(sock_fd, data_str.c_str(), buf_size) != buf_size) {
+        perror("ERROR writing to socket");
+    }
+
+    data_str.clear();
+//    std::cout << getpid() << " SENDING AFTER: " << data_str << std::endl;
+
+}
+
+json recv_json(char buffer[], int sock_fd) {
+
+    ssize_t read_bytes = read(sock_fd, buffer, BUFSIZE);
+
+    if (read_bytes > 0) {
+
+        try {
+
+            buffer[read_bytes] = '\0';
+            return json::parse(buffer);
+
+        } catch (json::parse_error &e) {
+
+            std::cout << BLUE << getpid() << DEF << " JSON PARSE ERROR " << e.what() << buffer << std::endl;
+            bzero(buffer, BUFSIZE);
+            return json{};
+
+        } catch (json::type_error &e) {
+
+            std::cout << getpid() << " JSON TYPE ERROR " << e.what() << buffer << std::endl;
+            return json{};
+
+        }
+
+    } else if (read_bytes < 0) {
+
+        std::cout << getpid() << " BUFFER READ ERROR" << std::endl;
+        perror("BUFFER READ ERROR");
+
+    } else {
+
+        std::cout << getpid() << " EMPTY BUFFER" << std::endl;
+        perror("EMPTY BUFFER");
+
+    }
+
+    return json{};
+
+}
+
+#endif
