478704ea8bf1f04f757fcd0ac68bc8d556e157d2
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-02-26 18:24:27 -0500

    restructure proto

--- dev/proto/deprecated/linksysc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -o errexit
-
-echo "Linking files from kernel examples..."
-ln -f $SYSC_KERNEL/examples/sysc/counter/counter.cpp sysc_counter.cpp
-ln -f $SYSC_KERNEL/examples/sysc/counter/counter_tb.cpp sysc_counter_tb.cpp
