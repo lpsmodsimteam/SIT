72e0073e92e2653bfeafa4571d2be8efea65d4d1
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-03-04 13:01:53 -0500

    properly link src files

--- src/sigutils.hpp
+++ src/sigutils.hpp
@@ -4,9 +4,14 @@
 #include <msgpack.hpp>
 #include <zmq.hpp>
 
+#include <iostream>
 #include <sstream>
 #include <unistd.h>
 #include <unordered_map>
+#include <utility>      // std::pair, std::make_pair
+
+#define SC_BIT_T 1
+#define SC_UINT_T 2
 
 class SignalHandler {
 
@@ -17,7 +22,10 @@ private:
     msgpack::packer<msgpack::sbuffer> m_packer;
     msgpack::unpacked m_unpacker;
     msgpack::sbuffer m_sbuf;
-    std::unordered_map<std::string, std::string> m_data;
+    std::unordered_map<std::string, std::pair<std::string, uint8_t >> m_data;
+
+    template<typename T>
+    std::string _to_string(const T &);
 
 public:
 
@@ -27,15 +35,21 @@ public:
 
     ~SignalHandler();
 
+    template<typename T>
+    void set(const std::string &, const T &, uint8_t = SC_BIT_T);
+
+    template<typename T>
+    T get(const std::string &);
+
     void send();
 
     void recv();
 
-    std::string &operator[](std::string);
-
 };
 
+
 /* -------------------- IMPLEMENTATIONS -------------------- */
+
 
 SignalHandler::SignalHandler(zmq::socket_t &socket) :
         m_socket(socket), m_packer(&m_sbuf) {
@@ -49,9 +63,40 @@ SignalHandler::~SignalHandler() {
 
 }
 
-std::string &SignalHandler::operator[](const std::string index) {
+template<typename T>
+std::string SignalHandler::_to_string(const T &value) {
 
-    return m_data[index];
+    std::ostringstream ss;
+    ss << value;
+    return ss.str();
+
+}
+
+
+template<typename T>
+void SignalHandler::set(const std::string &key, const T &value, uint8_t data_type) {
+
+    m_data[key].first = _to_string(value);
+    m_data[key].second = data_type;
+
+}
+
+
+template<typename T>
+T SignalHandler::get(const std::string &key) {
+
+    std::string value = m_data[key].first;
+    uint8_t data_t = m_data[key].second;
+
+    switch (data_t) {
+        case SC_BIT_T:
+            return value != "0";
+        case SC_UINT_T:
+            return std::stoi(value);
+        default:
+            throw std::invalid_argument("INVALID TYPE OR SOMETHING");
+
+    }
 
 }
 
@@ -71,31 +116,6 @@ void SignalHandler::recv() {
     m_socket.recv(&m_buf);
     msgpack::unpack(m_unpacker, (char *) (m_buf.data()), m_buf.size());
     m_unpacker.get().convert(*this);
-
-}
-
-/* -------------------- DECLARATIONS -------------------- */
-
-template<typename T>
-std::string _to_string(const T &);
-
-template<typename T>
-int _sc_signal_to_int(const T &);
-
-
-template<typename T>
-std::string _to_string(const T &value) {
-
-    std::ostringstream ss;
-    ss << value;
-    return ss.str();
-
-}
-
-template<typename T>
-int _sc_signal_to_int(const T &value) {
-
-    return std::stoi(_to_string(value));
 
 }
 
