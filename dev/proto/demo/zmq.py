85dd434d13a75e60dde45e69e7f4e4c1f696599f
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-03-12 17:16:32 -0400

    remove duplicated code from proto

--- dev/proto/demo/zmq.py
+++ dev/proto/demo/zmq.py
@@ -1,56 +1,21 @@
-from random import SystemRandom
-from string import ascii_uppercase, digits
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
 import os
 
-import sst
+PACKAGE_PARENT = '.'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-BASE_PATH = os.getcwd()
-CLOCK = "500MHz"
-sst.setProgramOption("stopAtCycle", "80ns")
+from util import galois_lfsr_comp, fib_lfsr_comp, galois_file, fib_file
 
-proto_comp = sst.Component("prototype", "proto.prototype")
-proto_comp.addParams({
-    "clock": CLOCK,
-})
-
-galois_lfsr_comp = sst.Component("galois_lfsr", "proto.galois_lfsr")
 # overide default parameters
 galois_lfsr_comp.addParams({
-    "proc": os.path.join(BASE_PATH, "galois_lfsr.o"),
-    "ipc_port": "ipc:///tmp/" + ''.join(SystemRandom().choice(ascii_uppercase + digits) for _ in range(8)),
-    "clock": CLOCK,
+    "ipc_port": "ipc:///tmp/" + galois_file,
 })
 
-fib_lfsr_comp = sst.Component("fib_lfsr", "proto.fib_lfsr")
 # overide default parameters
 fib_lfsr_comp.addParams({
-    "proc": os.path.join(BASE_PATH, "fib_lfsr.o"),
-    "ipc_port": "ipc:///tmp/" + ''.join(SystemRandom().choice(ascii_uppercase + digits) for _ in range(8)),
-    "clock": CLOCK,
+    "ipc_port": "ipc:///tmp/" + fib_file,
 })
-
-sst.Link("galois_reset").connect(
-    (galois_lfsr_comp, "galois_reset", "1ps"),
-    (proto_comp, "galois_reset", "1ps"),
-)
-sst.Link("galois_clock").connect(
-    (galois_lfsr_comp, "galois_clock", "1ps"),
-    (proto_comp, "galois_clock", "1ps"),
-)
-sst.Link("galois_data_out").connect(
-    (galois_lfsr_comp, "galois_data_out", "1ps"),
-    (proto_comp, "galois_data_out", "1ps"),
-)
-
-sst.Link("fib_reset").connect(
-    (fib_lfsr_comp, "fib_reset", "1ps"),
-    (proto_comp, "fib_reset", "1ps"),
-)
-sst.Link("fib_clock").connect(
-    (fib_lfsr_comp, "fib_clock", "1ps"),
-    (proto_comp, "fib_clock", "1ps"),
-)
-sst.Link("fib_data_out").connect(
-    (fib_lfsr_comp, "fib_data_out", "1ps"),
-    (proto_comp, "fib_data_out", "1ps"),
-)
--- src/sstscit.hpp
+++ src/sstscit.hpp
@@ -1,16 +1,10 @@
 #ifndef SSTSCIT_HPP
 #define SSTSCIT_HPP
 
-#include <iostream>
-
 #ifndef USE_ZMQ
-
 #include "socksigs.hpp"
-
 #else
-
 #include "zmqsigs.hpp"
-
 #endif
 
 #endif
