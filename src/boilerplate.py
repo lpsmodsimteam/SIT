6745b07c0d48c007ddbb44f36d27430a7e86abc1
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-03-19 12:06:02 -0400

    add zmq boilerplate generator

--- src/boilerplate.py
+++ src/boilerplate.py
@@ -6,38 +6,38 @@ import os
 
 class BoilerPlate():
 
-    def __init__(self, module, ipc, driver_templ_path, bbox_templ_path):
+    def __init__(self, module, lib, ipc, drvr_templ_path, bbox_templ_path,
+                 desc=""):
 
-        self.module = module
         if ipc in ("socks", "zmq"):
             self.ipc = ipc
         else:
             raise ValueError("Incorrect IPC protocol selected")
-        self.driver_templ_path = driver_templ_path
+
+        self.module = module
+        self.lib = lib
+        self.drvr_templ_path = drvr_templ_path
         self.bbox_templ_path = bbox_templ_path
+        self.desc = desc
 
         self.clocks = []
         self.inputs = []
         self.outputs = []
         self.inouts = []
         self.ports = []
-        self.driver_var_decl = ""
 
-        self.lib = ""
-        self.comp = ""
-        self.desc = ""
-        self.link = ""
-        self.link_desc = ""
+        self.drvr_decl = ""
 
-        self.bbox_var_decl = ""
-        self.bbox_var_init = ""
-        self.bbox_var_bind = ""
-        self.bbox_var_dest = ""
-        self.bbox_sender = ""
+        self.bbox_bind = ""
+        self.bbox_decl = ""
+        self.bbox_dest = ""
+        self.bbox_init = ""
+        self.bbox_links = []
         self.bbox_receiver = ""
+        self.bbox_sender = ""
 
         if self.ipc == "zmq":
-            self.driver_var_decl = """// Socket to talk to server
+            self.drvr_decl = """// Socket to talk to server
     zmq::context_t context(1);
     zmq::socket_t socket(context, ZMQ_REQ);
     socket.connect(argv[1]);
@@ -45,14 +45,14 @@ class BoilerPlate():
     // Initialize signal handlers
     ZMQReceiver sh_in(socket);
     ZMQTransmitter sh_out(socket);"""
-            self.bbox_var_decl = """zmq::context_t m_context;
+            self.bbox_decl = """zmq::context_t m_context;
     zmq::socket_t m_socket;
     ZMQReceiver m_sh_in;
     ZMQTransmitter m_sh_out;"""
-            self.bbox_var_init = """m_context(1), m_socket(m_context, ZMQ_REP),
+            self.bbox_init = """m_context(1), m_socket(m_context, ZMQ_REP),
       m_sh_in(m_socket), m_sh_out(m_socket),"""
-            self.bbox_var_bind = "m_socket.bind(m_ipc_port.c_str())"
-            self.bbox_var_dest = ""
+            self.bbox_bind = "m_socket.bind(m_ipc_port.c_str())"
+            self.bbox_dest = "m_socket.close();"
             self.bbox_sender = "m_sh_in"
             self.bbox_receiver = "m_sh_out"
 
@@ -60,7 +60,7 @@ class BoilerPlate():
 
         for port, port_type in ports.items():
             if port_type == "clock":
-                self.clocks.append(port)
+                self.clocks.append(port.split(" "))
             elif port_type == "input":
                 self.inputs.append(port.split(" "))
             elif port_type == "output":
@@ -76,15 +76,17 @@ class BoilerPlate():
 
         if "sc" in signal:
 
-            if "int" in signal or "uint" in signal or "bv" in signal:
-                return "<int>", int("".join(s for s in signal if s.isdigit()))
+            def __get_ints(sig):
+
+                return int("".join(s for s in sig if s.isdigit()))
+
+            if "int" in signal or "bv" in signal or "lv" in signal:
+                return "<int>", __get_ints(signal)
 
             if "bit" in signal or "logic" in signal:
-                return "<bool>", int("".join(s for s in signal if s.isdigit()))
+                return "<bool>", __get_ints(signal)
 
-        else:
-
-            return signal, 1
+        return signal, 1
 
     @staticmethod
     def __format(fmt, split_func, array, delim=";\n    "):
@@ -102,21 +104,21 @@ class BoilerPlate():
             lambda x: {"sig": x.split(" ")[-1]}, self.ports
         )
 
-    def get_clock(self):
+    def get_clock(self, drvr=True):
 
         return self.__format(
-            "sh_in.get_clock_pulse(\"{sig}\")",
-            lambda x: {"sig": x.split(" ")[-1]}, self.clocks
-        )
+            "{sig} = sh_in.get_clock_pulse(\"{sig}\")",
+            lambda x: {"sig": x[-1]}, self.clocks
+        ) if drvr else [["<sc_int<2>>", i[-1]] for i in self.clocks]
 
-    def get_inputs(self, driver=True):
+    def get_inputs(self, drvr=True):
 
         return self.__format(
             "{sig} = sh_in.get{type}(\"{sig}\")",
             lambda x: {
-                "sig": x[-1], "type": self.__parse_signal_type(x[0])[0]},
-            self.inputs, ";\n" + " " * 8
-        ) if driver else self.__format(
+                "sig": x[-1], "type": self.__parse_signal_type(x[0])[0]
+            }, self.inputs, ";\n" + " " * 8
+        ) if drvr else self.__format(
             "std::to_string(m_sh_in.get{type}(\"{sig}\"))",
             lambda x: {
                 "sig": x.split(" ")[-1],
@@ -124,9 +126,9 @@ class BoilerPlate():
             }, self.outputs, " +\n" + " " * 16
         )
 
-    def get_outputs(self, driver=True):
+    def get_outputs(self, drvr=True):
 
-        if driver:
+        if drvr:
 
             return self.__format(
                 "sh_out.set(\"{sig}\", {sig})",
@@ -134,47 +136,38 @@ class BoilerPlate():
                 self.outputs, ";\n" + " " * 8
             )
 
-        else:
+        ix = 2
+        sig_lens = []
+        bbox_inputs = self.inputs + self.get_clock(False)
+        for i in bbox_inputs:
+            sig_len = self.__parse_signal_type(i[0])[-1]
+            sig_lens.append((ix, sig_len))
+            ix += sig_len
 
-            ix = 2
-            sig_lens = []
-            for i in self.inputs:
-                sig_len = self.__parse_signal_type(i[0])[-1]
-                sig_lens.append((ix, sig_len))
-                ix += sig_len
+        return self.__format(
+            "m_sh_out.set(\"{sig}\", std::stoi(_data_in.substr({p}, {l})))",
+            lambda x: {
+                "sig": x[-1],
+                "p": sig_lens[bbox_inputs.index(x)][0],
+                "l": sig_lens[bbox_inputs.index(x)][-1]
+            }, bbox_inputs, ";\n" + " " * 8
+        )
 
-            return self.__format(
-                "m_sh_out.set(\"{sig}\", std::stoi(_data_in.substr({p}, {l})))",
-                lambda x: {
-                    "sig": x[-1],
-                    "p": sig_lens[self.inputs.index(x)][0],
-                    "l": sig_lens[self.inputs.index(x)][-1]
-                }, self.inputs, ";\n" + " " * 8
-            )
+    def generate_sc_drvr(self):
 
-    def generate_sc_driver(self):
-
-        if os.path.isfile(self.driver_templ_path):
-            with open(self.driver_templ_path) as template:
+        if os.path.isfile(self.drvr_templ_path):
+            with open(self.drvr_templ_path) as template:
                 return template.read().format(
                     module=self.module,
                     port_defs=self.get_port_defs(),
                     bindings=self.get_bindings(),
-                    var_decl=self.driver_var_decl,
+                    var_decl=self.drvr_decl,
                     clock=self.get_clock(),
                     inputs=self.get_inputs(),
                     outputs=self.get_outputs()
                 )
 
         raise FileNotFoundError("Driver boilerplate file not found")
-
-    def set_bbox(self, lib, comp, link, desc="", link_desc=""):
-
-        self.lib = lib
-        self.comp = comp
-        self.link = link
-        self.desc = desc
-        self.link_desc = link_desc
 
     def generate_bbox(self):
 
@@ -183,12 +176,12 @@ class BoilerPlate():
                 return template.read().format(
                     module=self.module,
                     lib=self.lib,
-                    comp=self.comp,
+                    comp=self.module,
                     desc=self.desc,
-                    var_decl=self.bbox_var_decl,
-                    var_init=self.bbox_var_init,
-                    var_bind=self.bbox_var_bind,
-                    var_dest=self.bbox_var_dest,
+                    var_decl=self.bbox_decl,
+                    var_init=self.bbox_init,
+                    var_bind=self.bbox_bind,
+                    var_dest=self.bbox_dest,
                     sender=self.bbox_sender,
                     receiver=self.bbox_receiver,
                     inputs=self.get_inputs(False),
@@ -201,26 +194,21 @@ class BoilerPlate():
 if __name__ == "__main__":
 
     BASE_DIR = os.getcwd()
-    DRIVER_TEMPL_PATH = os.path.join(BASE_DIR, "template", "driver.tmp")
+    DRVR_TEMPL_PATH = os.path.join(BASE_DIR, "template", "driver.tmp")
     BBOX_TEMPL_PATH = os.path.join(BASE_DIR, "template", "blackbox.tmp")
 
     galois = BoilerPlate(
         module="galois_lfsr",
+        lib="proto",
         ipc="zmq",
-        driver_templ_path=DRIVER_TEMPL_PATH,
+        desc="Simple 4-bit Galois Linear Feedback Shift Register",
+        drvr_templ_path=DRVR_TEMPL_PATH,
         bbox_templ_path=BBOX_TEMPL_PATH
     )
-
     galois.set_ports({
         "<bool> clock": "clock",
         "<bool> reset": "input",
-        "<bool> reset1": "input",
-        "<bool> reset2": "input",
-        "<sc_uint<32> > din": "input",
         "<sc_uint<4> > data_out": "output",
-        "<sc_uint<4> > data_out1": "output",
     })
-    print(galois.generate_sc_driver())
-
-    galois.set_bbox("proto", "galois_lfsr", "_link")
+    print(galois.generate_sc_drvr())
     print(galois.generate_bbox())
