3f2a0f2008f9ad193a7228732c868b0fc91d0f77
sabbirahm3d <sabbir0ahmed0@gmail.com>
2019-04-18 14:53:32 -0400

    refactor unit test scripts, remove deprecated options from simulation [ci skip]

--- examples/unit/demo/proto/socks.py
+++ examples/unit/demo/proto/socks.py
@@ -1,76 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from random import SystemRandom
-from string import ascii_uppercase, digits
 import os
+import sys
 
-import sst
+BASE_DIR = os.path.dirname(os.getcwd())
+SCRIPT_PATH = os.path.join(BASE_DIR, "demo")
+sys.path.append(SCRIPT_PATH)
+from params import unit_test
 
-BASE_PATH = os.getcwd()
-CLOCK = "1MHz"
-
-
-def get_rand_tmp():
-    return ''.join(
-        SystemRandom().choice(ascii_uppercase + digits) for _ in range(8)
-    )
-
-
-sst.setProgramOption("stopAtCycle", "50us")
-
-proto_comp = sst.Component("prototype", "proto.prototype")
-proto_comp.addParams({
-    "clock": CLOCK,
-})
-
-galois_lfsr_comp = sst.Component("galois_lfsr", "proto.galois_lfsr")
-# override default parameters
-galois_lfsr_comp.addParams({
-    "proc": os.path.join(BASE_PATH, "galois_lfsr.o"),
-    "ipc_port": "/tmp/" + get_rand_tmp(),
-    "clock": CLOCK,
-})
-
-galois_lfsr_comp2 = sst.Component("galois_lfsr2", "proto.galois_lfsr")
-# override default parameters
-galois_lfsr_comp2.addParams({
-    "proc": os.path.join(BASE_PATH, "galois_lfsr.o"),
-    "ipc_port": "/tmp/" + get_rand_tmp(),
-    "clock": CLOCK,
-})
-
-fib_lfsr_comp = sst.Component("fib_lfsr", "proto.fib_lfsr")
-# override default parameters
-fib_lfsr_comp.addParams({
-    "proc": os.path.join(BASE_PATH, "fib_lfsr.o"),
-    "ipc_port": "/tmp/" + get_rand_tmp(),
-    "clock": CLOCK,
-})
-
-sst.Link("galois_lfsr_din").connect(
-    (galois_lfsr_comp, "galois_lfsr_din", "1ps"),
-    (proto_comp, "galois_lfsr_din", "1ps"),
-)
-sst.Link("galois_lfsr_dout").connect(
-    (galois_lfsr_comp, "galois_lfsr_dout", "1ps"),
-    (proto_comp, "galois_lfsr_dout", "1ps"),
-)
-
-sst.Link("galois_lfsr_din2").connect(
-    (galois_lfsr_comp2, "galois_lfsr_din", "1ps"),
-    (proto_comp, "galois_lfsr_din", "1ps"),
-)
-sst.Link("galois_lfsr_dout2").connect(
-    (galois_lfsr_comp2, "galois_lfsr_dout", "1ps"),
-    (proto_comp, "galois_lfsr_dout", "1ps"),
-)
-
-sst.Link("fib_lfsr_din").connect(
-    (fib_lfsr_comp, "fib_lfsr_din", "1ps"),
-    (proto_comp, "fib_lfsr_din", "1ps"),
-)
-sst.Link("fib_lfsr_dout").connect(
-    (fib_lfsr_comp, "fib_lfsr_dout", "1ps"),
-    (proto_comp, "fib_lfsr_dout", "1ps"),
-)
+unit_test("sock", "prototype")
--- examples/unit/demo/proto/zmq.py
+++ examples/unit/demo/proto/zmq.py
@@ -1,51 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from random import SystemRandom
-from string import ascii_uppercase, digits
 import os
+import sys
 
-import sst
+BASE_DIR = os.path.dirname(os.getcwd())
+SCRIPT_PATH = os.path.join(BASE_DIR, "demo")
+sys.path.append(SCRIPT_PATH)
+from params import unit_test
 
-BASE_PATH = os.getcwd()
-CLOCK = "500MHz"
-sst.setProgramOption("stopAtCycle", "80ns")
-
-proto_comp = sst.Component("prototype", "proto.prototype")
-proto_comp.addParams({
-    "clock": CLOCK,
-})
-
-galois_lfsr_comp = sst.Component("galois_lfsr", "proto.galois_lfsr")
-# overide default parameters
-galois_lfsr_comp.addParams({
-    "proc": os.path.join(BASE_PATH, "galois_lfsr.o"),
-    "ipc_port": "ipc:///tmp/" + ''.join(SystemRandom().choice(ascii_uppercase + digits) for _ in range(8)),
-    "clock": CLOCK,
-})
-
-fib_lfsr_comp = sst.Component("fib_lfsr", "proto.fib_lfsr")
-# overide default parameters
-fib_lfsr_comp.addParams({
-    "proc": os.path.join(BASE_PATH, "fib_lfsr.o"),
-    "ipc_port": "ipc:///tmp/" + ''.join(SystemRandom().choice(ascii_uppercase + digits) for _ in range(8)),
-    "clock": CLOCK,
-})
-
-sst.Link("galois_lfsr_din").connect(
-    (galois_lfsr_comp, "galois_lfsr_din", "1ps"),
-    (proto_comp, "galois_lfsr_din", "1ps"),
-)
-sst.Link("galois_lfsr_dout").connect(
-    (galois_lfsr_comp, "galois_lfsr_dout", "1ps"),
-    (proto_comp, "galois_lfsr_dout", "1ps"),
-)
-
-sst.Link("fib_lfsr_din").connect(
-    (fib_lfsr_comp, "fib_lfsr_din", "1ps"),
-    (proto_comp, "fib_lfsr_din", "1ps"),
-)
-sst.Link("fib_lfsr_dout").connect(
-    (fib_lfsr_comp, "fib_lfsr_dout", "1ps"),
-    (proto_comp, "fib_lfsr_dout", "1ps"),
-)
+unit_test("zmq", "prototype")
