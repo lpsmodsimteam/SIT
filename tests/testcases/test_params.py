6c2a217decf1caaba5e03c225de699ae79ab81ca
sabbirahm3d <sabbir0ahmed0@gmail.com>
2020-01-07 15:20:37 -0500

    add skeleton scripts

--- tests/testcases/test_params.py
+++ tests/testcases/test_params.py
@@ -39,6 +39,9 @@ def setup(hdl, ipc, comp):
         proc = os.path.join(BASE_PATH, "ram.o")
     elif hdl == "pyrtl":
         proc = os.path.join(os.path.dirname(BASE_PATH), ipc, "blackboxes", "ram_driver.py")
+    elif hdl == "chisel":
+        print(BASE_PATH)
+        proc = os.path.join("sbt 'test:runMain ram.RAMMain'")
     else:
         raise NotImplementedError()
     ram_comp.addParams({
